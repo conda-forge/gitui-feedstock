d1660ade2677403e1348144d06e41a18f2e6d68f
Daniel Bast <2790401+dbast@users.noreply.github.com>
2024-10-26 15:17:06 +0200

    Enable osx-arm64 / linux-{aarch64, ppc64le} builds, modernize recipe

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,8 +1,17 @@
-echo ON
-cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
-if errorlevel 1 exit 1
-cargo build --release --locked
-if errorlevel 1 exit 1
-cargo install --path . --root %PREFIX% --locked
-del %PREFIX%\.crates2.json
-del %PREFIX%\.crates.toml
+@echo on
+
+set CARGO_PROFILE_RELEASE_STRIP=symbols
+set CARGO_PROFILE_RELEASE_LTO=fat
+
+:: dump licenses
+cargo-bundle-licenses ^
+    --format yaml ^
+    --output "%SRC_DIR%\THIRDPARTY.yml" ^
+    || exit 1
+
+:: build
+cargo install --locked ^
+    --root "%PREFIX%" ^
+    --path . ^
+    --no-track ^
+    || exit 1
